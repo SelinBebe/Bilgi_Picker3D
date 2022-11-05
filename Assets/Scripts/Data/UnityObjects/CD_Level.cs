12e758f5442daef56b1c62e2eec3632c724ed0c6
SelinBebe <101172954+SelinBebe@users.noreply.github.com>
2022-11-05 11:19:57 +0300

    Data klasöründeki kodların düzenlemesi yapıldı, gereksiz dosyalar silindi.

--- Assets/Scripts/Data/UnityObjects/CD_Level.cs
+++ Assets/Scripts/Data/UnityObjects/CD_Level.cs
@@ -1,9 +1,12 @@
 using System.Collections.Generic;
+using Data.ValueObjects;
 using UnityEngine;
-[CreateAssetMenu(fileName = "CD_Level" , menuName = "Picker3D/CD_Level", order =1)]
-//menü name picker3d is cd level olu?tur something like that 
 
-public class CD_Level : ScriptableObject
+namespace Data.UnityObjects
 {
-   public List<LevelData> Levels = new List<LevelData>();
+   [CreateAssetMenu(fileName = "CD_Level", menuName = "Picker3D/CD_Level", order = 1)]
+   public class CD_Level : ScriptableObject
+   {
+      public List<LevelData> Levels = new List<LevelData>();
+   }
 }
