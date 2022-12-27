4c330f7e7c99426b789e135b6c596b25e593414a
SelinBebe <101172954+SelinBebe@users.noreply.github.com>
2022-12-27 14:25:42 +0300

    her şey çözüldü çok şükür elhamdülilah Allahu Ekber Amin Allahın ne istediğimi biliyorsun amin

--- Assets/Scripts/Controllers/Player/PlayerMeshController.cs
+++ Assets/Scripts/Controllers/Player/PlayerMeshController.cs
@@ -16,6 +16,7 @@ namespace Controllers.Player
         [SerializeField] private PlayerManager manager;
         [SerializeField] private new Renderer renderer;
         [SerializeField] private TextMeshPro scaleText;
+        [SerializeField] private ParticleSystem Confetti;
 
         #endregion
 
@@ -42,6 +43,11 @@ namespace Controllers.Player
             scaleText.DOFade(1, 0f).SetEase(Ease.Flash).OnComplete(() => scaleText.DOFade(0, 0).SetDelay(.65f));
             scaleText.rectTransform.DOAnchorPosY(.85f, .65f).SetRelative(true).SetEase(Ease.OutBounce).OnComplete(() =>
                 scaleText.rectTransform.DOAnchorPosY(-.85f, .65f).SetRelative(true));
+        }
+
+        internal void PlayConfettiParticle()
+        {
+            Confetti.Play();
         }
 
         internal void OnReset()
